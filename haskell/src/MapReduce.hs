771ac717aa1e70df808dffc960ddba356f5f6b77
brady.ouren <brady.ouren@gmail.com>
2016-05-13 16:38:19 -0700

    streaming text lines

--- haskell/src/MapReduce.hs
+++ haskell/src/MapReduce.hs
@@ -2,16 +2,24 @@
 module MapReduce where
 
 import           Pipes
+import           Pipes.Prelude        as Pipes
+import           Pipes.Prelude.Text   as Text
 import           Pipes.Safe
-import qualified Pipes.Text    as Text
-import qualified Pipes.Text.IO as Text
+import qualified Pipes.Text           as Text
+import qualified Pipes.Text.IO        as Text
+import           System.FilePath.Glob
 
 doIt :: IO ()
 doIt = runSafeT $ runEffect $
-        Text.readFile "../tmp/tweets/tweets_aa"
+        all_files
     >-> filter_and_group
-    >-> sort_by_freq_and_hood
+    -- >-> sort_by_freq_and_hood
     >-> Text.stdout
 
+all_files = do
+  globbed <- liftIO $ globDir1 (compile "tweets_*") "../tmp/tweets"
+  for (each globbed) Text.readFileLn
+
 filter_and_group = undefined
+  -- Pipes.filter (\a -> any ((==) "knicks") a)
 sort_by_freq_and_hood = undefined
