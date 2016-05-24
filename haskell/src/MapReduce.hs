55188b4fd3e486754652d5f77215c4177bc042b8
brady.ouren <brady.ouren@gmail.com>
2016-05-23 21:43:35 -0700

    fix MonadSafe types

--- haskell/src/MapReduce.hs
+++ haskell/src/MapReduce.hs
@@ -1,46 +1,50 @@
 {-# LANGUAGE OverloadedStrings #-}
 module MapReduce where
 
-import           Control.Monad        (void)
-import qualified Data.Map.Strict      as Map
-import qualified Data.Text            as T
-import           Pipes
-import           Pipes.Prelude        as P
-import           Pipes.Prelude.Text   as Text
-import           Pipes.Safe
-import qualified Pipes.Text           as Text
-import qualified Pipes.Text.IO        as Text
-import           System.FilePath.Glob
-import           System.IO
+import Control.Monad (void)
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import Pipes
+import Pipes.Prelude as P
+import Pipes.Prelude.Text as Text
+import Pipes.Safe
+import qualified Pipes.Text as Text
+import qualified Pipes.Text.IO as Text
+import System.FilePath.Glob
+import System.IO
 
--- doIt :: IO ()
+type HoodMap = Map.Map Text.Text Integer
+
+doIt :: IO ()
+doIt = do
+  map <- runSafeT doPipe
+  return $ printMap map
+-- doIt :: MonadSafe m => m ()
 -- doIt = printMap =<< doPipe
 
-doPipe :: IO (Map.Map Text.Text Int)
-doPipe = runEffect hoodProducer
+doPipe :: (MonadSafe m) => m HoodMap
+doPipe = foldIntoMap hoodProducer
 
-foldIntoMap :: (MonadSafe m) => Producer Text.Text m () -> Map.Map Text.Text Int
-foldIntoMap = P.fold incHood Map.empty id
-  where
-    incHood m hood = Map.insertWith (+) hood 1 m
+foldIntoMap :: (MonadSafe m) => Producer Text.Text m () -> m HoodMap
+foldIntoMap = P.fold incHood (Map.empty :: HoodMap) id
+  where incHood m hood = Map.insertWith (+) hood 1 m
 
-hoodProducer :: Effect IO (Map.Map Text.Text Int)
+hoodProducer :: (MonadSafe m) => Producer Text.Text m ()
 hoodProducer =
           getInputFiles
       >-> mapper
-      >-> foldIntoMap
 
-getInputFiles :: (MonadSafe m) => Producer Text.Text m Text.Text
+getInputFiles :: (MonadSafe m) => Producer Text.Text m ()
 getInputFiles = do
   globbed <- liftIO $ globDir1 (compile "tweets_*") "../tmp/tweets"
   for (each globbed) Text.readFileLn
 
-mapper :: (MonadSafe m) => Pipe Text.Text Text.Text m Text.Text
+mapper :: (MonadSafe m) => Pipe Text.Text Text.Text m ()
 mapper = P.filter (\line -> "knicks" `T.isInfixOf` line)
-     >-> P.map (second . T.split ((==) '\t'))
+     >-> P.map (second . T.split ('\t' ==))
 
-printMap :: Map.Map Text.Text Int -> ()
-printMap = void $ Map.traverseWithKey (\k v -> Prelude.print (k,v))
+printMap :: HoodMap -> ()
+printMap = void $ Map.traverseWithKey $ curry Prelude.print --(\k v -> Prelude.print (k,v))
 
 -- writeFileWith res = Text.writeFile "../tmp/haskell_output" totals
 -- writeToFile p = p >~ openFile "something.txt" WriteMode >>= P.toHandle
